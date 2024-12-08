2ee4db4115103f6c928264e682e4e25d7a5bfd60
alex.mcewan <58659503+Baalekker@users.noreply.github.com>
2024-12-08 22:54:09 +0100

    python mock backend is compatible with and without container

--- python plots api/mockbackend/backend.py
+++ python plots api/mockbackend/backend.py
@@ -8,8 +8,10 @@ def send_get_request():
     print(f"Response from API: {response.text}")
 
 def send_post_request(functionstr, title):
-    response = requests.post("http://localhost:5000/plot", json={"function": functionstr, "title":title})
-    if response.status_code != 200: 
+    try: 
+        response = requests.post("http://localhost:5000/plot", json={"function": functionstr, "title":title})
+
+    except:
         response = requests.post("http://api:5000/plot", json={"function": functionstr, "title":title})
 
     if response.status_code == 200:
@@ -23,7 +25,7 @@ def send_post_request(functionstr, title):
             f.write(image_data)
 
 
-send_post_request("sin(x) + x", "alt")
+send_post_request("sin(x) + 4", "title")
 
 
 
